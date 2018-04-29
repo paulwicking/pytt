40196c389d900f6b88ab4e8d307e09f1e1dc2b81
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2018-04-29 15:35:01 +0200

    Add route for api end-point

--- training_tracker/web_app.py
+++ training_tracker/web_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from training_tracker import MODULE_VERSION, MODULE_NAME
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({'message': 'Hello world'})
+
+
+@app.route('/api/')
+def api_home():
+    return jsonify({'message': '%s %s API' % (MODULE_NAME, MODULE_VERSION)})
 
 
 if __name__ == '__main__':
