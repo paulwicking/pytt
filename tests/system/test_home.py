253a937803ac74193472e2f9de6a88958c66abb8
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2018-04-29 10:26:52 +0200

    Add comment

--- tests/system/test_home.py
+++ tests/system/test_home.py
@@ -10,6 +10,7 @@ class TestHome(TestCase):
 
             self.assertEqual(response.status_code, 200)
             self.assertEqual(
+                #  Remember to decode to UTF-8 for Python 3.4 and 3.5
                 json.loads(response.get_data().decode('utf-8')),
                 {'message': 'Hello world'}
             )
