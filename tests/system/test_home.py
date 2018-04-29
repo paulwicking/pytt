0efb4b127ec87e58fdff080c52ac3b60661361cb
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2018-04-29 09:54:36 +0200

    Flask: Hello world

---
+++ tests/system/test_home.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from training_tracker.web_app import app
+import json
+
+
+class TestHome(TestCase):
+    def test_home(self):
+        with app.test_client() as client:
+            response = client.get('/')
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                json.loads(response.get_data()),
+                {'message': 'Hello world'}
+            )
