b90a2bede3c9119ff59e24a7f72321ab3639ace7
Paul Wicking <paulwicking@gmail.com>
2018-06-01 18:55:29 +0200

    Create basic package for internal distribution

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='trainingtracker',
+    version='2018.6.1',
+    packages=find_packages(),
+    url='http://wowsu.ch/training',
+    license='MIT',
+    author='Paul Wicking',
+    author_email='paulwicking@gmail.com',
+    description='Track your powerlifting training progress.'
+)
+
