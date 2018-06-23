f8cdb09d3a232e4227d3fbe889d98aed92b1c219
Paul Wicking <paulwicking@gmail.com>
2018-06-23 09:04:42 +0200

    Moving tests around in source file
    Moving incomplete tests and tests subject to upcoming change
    for better readability.

--- tests/test_workouts.py
+++ tests/test_workouts.py
@@ -5,39 +5,6 @@ from training_tracker.workouts import Exercise, Workout
 
 
 class TestWorkouts(TestCase):
-    def test_create_set(self):
-        # squats = Exercise("Squat")
-        # target_weight = 100
-        # sets = 5
-        # result = workouts.create_set(exercise, sets, target_weight)
-        pass
-
-    def test_can_get_list_of_exercises_from_workout(self):
-        test_workout = Workout()
-        test_workout.add_exercise("Squat", 5, 100)
-
-        result = test_workout.exercise_list()
-
-        self.assertEqual(len(result), 1)
-
-    def test_can_add_new_exercise_to_workout(self):
-        test_workout = Workout()
-        test_workout.add_exercise("Squat", 5, 100)
-        
-        expected = [{'Exercise': 'Squat',
-                     'Sets': [(1, 60),
-                              (2, 70),
-                              (3, 80),
-                              (4, 90),
-                              (5, 100)
-                             ],
-                     'Description': 'Squats are awesome',
-                     }
-                    ]
-        result = test_workout.exercise_list()
-        
-        self.assertEqual(expected, result)
-
     def test_can_save_workouts(self):
         with patch('builtins.open', mock_open(read_data='data')) as mock_file:
             test_workout = Workout()
@@ -71,3 +38,36 @@ class TestWorkouts(TestCase):
 
                 with self.assertRaises(IOError):
                     test_workout.save_workout()
+
+    def test_create_set(self):
+        # squats = Exercise("Squat")
+        # target_weight = 100
+        # sets = 5
+        # result = workouts.create_set(exercise, sets, target_weight)
+        pass
+
+    def test_can_get_list_of_exercises_from_workout(self):
+        test_workout = Workout()
+        test_workout.add_exercise("Squat", 5, 100)
+
+        result = test_workout.exercise_list()
+
+        self.assertEqual(len(result), 1)
+
+    def test_can_add_new_exercise_to_workout(self):
+        test_workout = Workout()
+        test_workout.add_exercise("Squat", 5, 100)
+
+        expected = [{'Exercise': 'Squat',
+                     'Sets': [(1, 60),
+                              (2, 70),
+                              (3, 80),
+                              (4, 90),
+                              (5, 100)
+                              ],
+                     'Description': 'Squats are awesome',
+                     }
+                    ]
+        result = test_workout.exercise_list()
+
+        self.assertEqual(expected, result)
