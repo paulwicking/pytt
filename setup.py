88afa701c66c02287545927da3edbca085cd517f
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2018-06-28 21:07:44 +0200

    Use normalized version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as in_file:
 
 setup(
     name='pytt',
-    version='2018.6.1.alpha2',
+    version='2018.6.1a2',
     packages=find_packages(),
     url='https://www.trainingtracker.io/',
     license='MIT',
